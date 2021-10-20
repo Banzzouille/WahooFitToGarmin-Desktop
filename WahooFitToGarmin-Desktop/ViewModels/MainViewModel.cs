a8df9d760ba3f7a77d6b5301459af22b4a4a9327
Florian BARLETTA <fbarlett@itron.com>
2021-10-20 19:43:34 +0200

    adding the filewatcher

--- WahooFitToGarmin-Desktop/ViewModels/MainViewModel.cs
+++ WahooFitToGarmin-Desktop/ViewModels/MainViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows;
+using System.Windows.Threading;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
 using WahooFitToGarmin_Desktop.Helpers;
 
@@ -7,12 +10,43 @@ namespace WahooFitToGarmin_Desktop.ViewModels
 {
     public class MainViewModel : ObservableObject
     {
+        private string wahooFolder;
         public ObservableCollection<LogEntry> LogEntries { get; set; }
+
         public MainViewModel()
         {
-            LogEntries = new ObservableCollection<LogEntry>();
-            LogEntries.Add(new LogEntry("Starting ......."));
+            LogEntries = new ObservableCollection<LogEntry> { new LogEntry("Starting .......") };
+            DumpSettings();
 
+
+            var fw = new FileSystemWatcher
+            {
+                Filter = "*.fit",
+                Path = wahooFolder,
+                EnableRaisingEvents = true,
+                IncludeSubdirectories = false
+            };
+            fw.Created += fileIsComing;
+        }
+
+        private void fileIsComing(object sender, FileSystemEventArgs e)
+        {
+            Log($"A new file is coming => {e.Name}");
+        }
+
+        private void DumpSettings()
+        {
+            wahooFolder = App.Current.Properties["WahooDropBoxFolder"].ToString();
+            if (string.IsNullOrEmpty(wahooFolder))
+                Log("Please select folder to watch for in settings");
+
+            Log($"Wahoo folder to watch for : {wahooFolder}");
+        }
+
+        private void Log(string message)
+        {
+            Application.Current.Dispatcher.Invoke(
+                () => { LogEntries.Add(new LogEntry(message)); });
         }
     }
 }
